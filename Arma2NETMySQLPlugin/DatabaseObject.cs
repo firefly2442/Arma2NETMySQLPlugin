c8756d6adcb69566fbc58eb5f740e9562b354f3c
firefly2442 <firefly2442@gmail.com>
2012-06-13 14:46:14 -0500

    updated code for use with new 6.5.4 MySQL Connector and problem with stored procedures

--- Arma2NETMySQLPlugin/DatabaseObject.cs
+++ Arma2NETMySQLPlugin/DatabaseObject.cs
@@ -30,7 +30,17 @@ namespace Arma2NETMySQLPlugin
 
         private string getFormattedConnectionString()
         {
-            return "Server = " + ipaddress + "; Port = " + port + "; Database = " + databasename + "; Uid = " + username + "; Pwd = " + password + ";";
+            //With new (6.5.4) versions of the MySQL Connector, it was throwing an error when running the stored procedures:
+            //Unable to retrieve stored procedure metadata for routine.  Either grant  SELECT privilege to mysql.proc for this user or use "check parameters=false" with  your connection string.
+            //Therefore, check parameters is now false
+            //http://lists.mysql.com/commits/136669
+            /*  "We have made the Use Procedure Bodies flag obsolete and introduced the Check Parameters option.  They server mainly the
+                same purpose.  The Check Parameters option is true by default.  Setting to false tells
+                Connector/Net to not fetch any routine or parameter metadata and to simply trust what the user has specified.  This can greatly
+                increase performance but it also puts significant pressure on the application developer to give the parameters in the right
+                order."
+             */
+            return "Server = " + ipaddress + "; Port = " + port + "; Database = " + databasename + "; Uid = " + username + "; Pwd = " + password + ";CheckParameters=false";
         }
     }
 }
