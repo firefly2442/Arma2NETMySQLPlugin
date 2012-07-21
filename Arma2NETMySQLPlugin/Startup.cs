0675e62e07131d2f9282a97bd76d0327a1529387
firefly2442 <firefly2442@gmail.com>
2012-07-21 18:43:24 -0500

    oops, use correct utils version for version number

--- Arma2NETMySQLPlugin/Startup.cs
+++ Arma2NETMySQLPlugin/Startup.cs
@@ -28,7 +28,7 @@ namespace Arma2NETMySQLPlugin
                 //http://stackoverflow.com/questions/64602/what-are-differences-between-assemblyversion-assemblyfileversion-and-assemblyin
                 Logger.addMessage(Logger.LogType.Info, "Version number: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
 
-                Logger.addMessage(Logger.LogType.Info, "Compiled against Arma2NET Version: " + Environment.Version);
+                Logger.addMessage(Logger.LogType.Info, "Compiled against Arma2NET Version: " + Utils.Version);
 
                 //Load in Databases.txt file
                 //This also sets up the SQLProvider associated with the database
